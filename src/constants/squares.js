38e157254d4f5adf06879cc45935100bfe389db8
Fanaticys <dmkndrtnk@gmail.com>
2018-04-23 00:19:47 +0300

    --amend

--- src/constants/squares.js
+++
@@ -1,8 +0,0 @@
-export const UPDATE = 'UPDATE';
-export const RESIZE = 'RESIZE';
-export const LIVEN_UP = 'LIVEN_UP';
-export const PAUSE = 'PAUSE';
-export const RESUME = 'RESUME';
-export const FASTER = 'FASTER';
-export const SLOWER = 'SLOWER';
-export const CLEAR = 'CLEAR';
--- src/reducers/index.js
+++
@@ -1,5 +0,0 @@
-import { combineReducers } from 'redux';
-
-import squares from './squaresReducer';
-
-export default combineReducers({ squares });
